30db24b9979d757c1d70fefe35444a1376249407
Branta Jay Mikulecky <brad@Brantas-MacBook-Air.local>
2026-03-30 09:33:49 -0500

    finalized processing cost analysis

--- starter_code.py
+++ starter_code.py
@@ -35,14 +35,30 @@ def linear_search(data, target):
     """
     # TODO: Implement linear search that loops through each element and returns its index if found and -1 if not found.
     
-    pass # Delete pass and write your code here
-
+   # Delete pass and write your code here
+    for i in range(len(data)):
+        if data[i] == target:
+            return i
+    return -1
 
 # ============================================================================
 # PART 2: Binary Search (Iterative)
 # ============================================================================
 
 def binary_search_iterative(data, target):
+    left = 0
+    right = len(data) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if data[mid] == target:
+            return mid
+        elif data[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
+
     """
     Search for target in SORTED data using iterative binary search.
     
@@ -67,7 +83,7 @@ def binary_search_iterative(data, target):
     """
     # TODO: Implement iterative binary search that uses iteration to find the target. Return the index if found and -1 if not found.
     
-    pass # Delete pass and write your code here
+# Delete pass and write your code here
 
 
 # ============================================================================
@@ -104,8 +120,16 @@ def binary_search_recursive(data, target, left=None, right=None):
     
     # TODO: Implement recursive binary search that uses recursion to find the target. Return the index if found and -1 if not found. Note that default parameters are already handled above.
 
-    
-    pass # Delete pass and write your code here
+    if left > right:
+        return -1
+    mid = (left + right) // 2
+    if data[mid] == target:
+        return mid
+    if data[mid] < target:
+        return binary_search_recursive(data, target, mid + 1, right)
+    else:
+        return binary_search_recursive(data, target, left, mid - 1)
+    # Delete pass and write your code here
 
 
 # ============================================================================
@@ -283,8 +307,8 @@ if __name__ == "__main__":
     
     # Uncomment these as you complete each part:
     
-    # test_search_correctness()
-    # benchmark_all_datasets()
-    # analyze_preprocessing_costs()
+    test_search_correctness()
+    benchmark_all_datasets()
+    analyze_preprocessing_costs()
     
     print("\n⚠ Uncomment the test functions in the main block to run benchmarks!")
